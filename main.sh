92f5fb5747540c934b3f46c7caa1a62867e9eddf
ahmed makhlouf <ahmedhamada496@gmail.com>
2025-07-19 18:54:53 +0300

    Refactor database operation scripts path to use SCRIPT_DIR variable

--- main.sh
+++ main.sh
@@ -1,21 +1,22 @@
 #! /usr/bin/bash
 
 PS3="Select a database operation by number: "
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 select option in "Create Database" "List Databases" "Connect to Database" "Drop Database" "Exit"
 do
 case $option in
     "Create Database")
-        . ./db_operations/create_database
+        . "$SCRIPT_DIR/db_operations/create_database"
         ;;
     "List Databases")
-        . ./db_operations/list_databases
+        . "$SCRIPT_DIR/db_operations/list_databases"
         ;;
     "Connect to Database")
-        . ./db_operations/connect_database
+        . "$SCRIPT_DIR/db_operations/connect_database"
         ;;
     "Drop Database") 
-        . ./db_operations/drop_database
+        . "$SCRIPT_DIR/db_operations/drop_database"
         ;;  
     Exit)
         echo "Exiting..."
